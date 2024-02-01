f920c804b1d8ea582284ed9d082306eb18e33fd0
Dominik Kuzinowicz <47429567+dkuzi@users.noreply.github.com>
2024-02-01 13:44:00 +0100

    Add files via upload

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,48 @@
+push!(LOAD_PATH,"../src/")
 using Documenter
 using AVI
+
+# makedocs(
+#     sitename="AVI.jl",
+#     modules=[AVI],
+#     format=Documenter.HTML(repolink="https://github.com/ZIB-IOL/AVI.jl.git",
+#                             prettyurls = get(ENV, "CI", nothing) == "true",
+#     ),
+#     pages=[
+#         "Home" => "index.md",
+#         "How does it work?" => "basics.md",
+#         "Obtaining a transformation" => [
+#                                         "how_to_run.md",
+#                                         "docs_oavi_transformation.md",
+#                                         "docs_custom_oracle.md",
+#                                         "docs_vca_transformation.md"
+#                                         ],
+#         "Printing polynomials" => "print_polynomials.md",
+#         "API Reference" => ["reference/0_reference.md",
+#                             "reference/1_algorithms.md",
+#                             "reference/2_oracles.md",
+#                             "reference/3_border.md",
+#                             "reference/4_terms_and_polys.md",
+#                             "reference/5_auxiliary_funcs.md"
+#                             ]
+#     ],
+#     warnonly=true
+# )
+
+
+# deploydocs(
+#             repo="github.com/ZIB-IOL/AVI.jl.git",
+#             devbranch="main",
+#             push_preview=true
+# )
 
 makedocs(
     sitename="AVI.jl",
     modules=[AVI],
-    format=Documenter.HTML(repolink="https://github.com/ZIB-IOL/AVI.jl.git"),
+    format=Documenter.HTML(repolink="https://github.com/dkuzi/FileSaver.git",
+                            prettyurls = get(ENV, "CI", nothing) == "true",
+                            edit_link="main",
+    ),
     pages=[
         "Home" => "index.md",
         "How does it work?" => "basics.md",
@@ -28,11 +66,9 @@ makedocs(
 
 
 deploydocs(
-            repo="github.com/ZIB-IOL/AVI.jl.git",
+            repo="github.com/dkuzi/FileSaver.git",
             devbranch="main",
-            devurl="dev",
+            push_preview=true,
             target="build",
-            branch="gh-pages",
-            versions = ["stable" => "v^", "v#.#" ],
-            push_preview=true
+
 )
